4671b7b3c741292cde2eef34315a46aaaecae2e2
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-10-02 23:12:49 +0530

    /

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -14,6 +14,8 @@ export const addItemsToCart = (id, quantity) => async (dispatch, getState) => {
     payload: {
       product: data.product._id,
       
+
+      
       name: data.product.name,
       price: data.product.price,
       image: data.product.images[0].url,
