1d102951cfe0e310447b586994a50c598a4746e4
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-11-27 21:00:57 +0530

    /

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -6,7 +6,6 @@ import {
 import axios from "axios";
 
 
-
 // Add to Cart
 export const addItemsToCart = (id, quantity) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/v1/product/${id}`);
